868e629b88ec7d46f4861a6d7a19b17dd5ee0d00
steeven-js <jacques.steeven@gmail.com>
2023-10-27 10:55:25 -0400

    rename

--- ios/PlantJsprodTests/PlantJsprodTests.m
+++ ios/PlantJsprodTests/PlantJsprodTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PlantMartTests : XCTestCase
+@interface PlantJsprodTests : XCTestCase
 
 @end
 
-@implementation PlantMartTests
+@implementation PlantJsprodTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
